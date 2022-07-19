85ed82e97ba3e78030c49143df5f1ad08fccd34d
TranAnhVu0411 <72057891+TranAnhVu0411@users.noreply.github.com>
2022-07-19 13:55:16 +0700

    adding average fps calculation

--- face_detector/realtime_face_detector_hog.py
+++ face_detector/realtime_face_detector_hog.py
@@ -3,7 +3,21 @@ import dlib
 import time
 import imutils
 from imutils.video import VideoStream
+import time
+import numpy as np
+
+# used to record the time when we processed last frame
+prev_frame_time = 0
+ 
+# used to record the time at which we processed current frame
+new_frame_time = 0
+
+# list of fps in one frame
+fps_list = []
+
 vs = VideoStream(src=0).start()
+
+# change from (left, top, right, bottom) to (x, y, w, h)
 def convert_and_trim_bb(image, rect):
     startX = rect.left()
     startY = rect.top()
@@ -21,26 +35,39 @@ def convert_and_trim_bb(image, rect):
     return (startX, startY, w, h)
 detector = dlib.get_frontal_face_detector()
 time.sleep(2.0)
+
 while True:
-    # grab the frame from the video stream, resize it, and convert it
-	# to grayscale
+    # grab the frame from the video stream, resize it
     frame = vs.read()
     frame = imutils.resize(frame, width=500)
-    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
 	# perform face detection
     rects = detector(frame, 0)
-    rects = [convert_and_trim_bb(image, r) for r in rects]
+    rects = [convert_and_trim_bb(frame, r) for r in rects]
 
     # loop over the bounding boxes
     for (x, y, w, h) in rects:
 		# draw the face bounding box on the image
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    
+    # save fps
+    new_frame_time = time.time()
+    fps = 1/(new_frame_time-prev_frame_time)
+    fps_list.append(fps)
+    prev_frame_time = new_frame_time
+
     # show the output frame
-    cv2.imshow("Frame", image)
+    cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
-	# if the `q` key was pressed, break from the loop
-    if key == ord("q"):
+
+	# if the `q` key was pressed
+    # or limit the frame count (only capture 500 frames in this case)
+    # break from the loop
+    if key == ord("q") or len(fps_list)==500:
     	break
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
+# show avg fps
+print("average fps")
+print(np.array(fps_list).mean())
--- face_detector/realtime_face_detector_mtcnn.py
+++ face_detector/realtime_face_detector_mtcnn.py
@@ -6,17 +6,24 @@ from torch_mtcnn import detect_faces
 import time
 import imutils
 from imutils.video import VideoStream
+
+# used to record the time when we processed last frame
+prev_frame_time = 0
+ 
+# used to record the time at which we processed current frame
+new_frame_time = 0
+
+# list of fps in one frame
+fps_list = []
 vs = VideoStream(src=0).start()
 
 time.sleep(2.0)
 while True:
-    # grab the frame from the video stream, resize it, and convert it
-	# to grayscale
+    # grab the frame from the video stream and resize it
     frame = vs.read()
     frame = imutils.resize(frame, width=500)
     img = Image.fromarray(np.uint8(frame)).convert('RGB')
-    # print(frame.shape)
-    # img=frame
+
 	# perform face detection
     try:
         bounding_boxes, landmarks = detect_faces(img)
@@ -33,14 +40,26 @@ while True:
                 x=int(i[j])
                 y=int(i[j+5])
                 cv2.circle(frame, (x,y), radius=3, color=(0, 0, 255), thickness=-1)
+
+        # save fps
+        new_frame_time = time.time()
+        fps = 1/(new_frame_time-prev_frame_time)
+        fps_list.append(fps)
+        prev_frame_time = new_frame_time
+
         # show the output frame
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
-        # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
+        # if the `q` key was pressed
+        # or limit the frame count (only capture 500 frames in this case)
+        # break from the loop
+        if key == ord("q") or len(fps_list)==500:
             break
     except:
         print("Error")
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
+# show avg fps
+print("average fps")
+print(np.array(fps_list).mean())
